246c762bc95ed24a25f9d8cdeffdcbf6ae4c4bcc
BijjamVijayReddy <bijjamvijayreddy@gmail.com>
2025-11-12 11:20:03 +0530

    Variables in Java

---
+++ Two.java
@@ -0,0 +1,27 @@
+
+
+ class Two {
+
+    // method 
+
+    void  m2 () {
+       System.out.println("Hi Vijay Very Good Morning From Java Language ");
+    }
+
+    void m3 () {
+      System.out.println("Second Method is Working Now");
+    }
+
+     void m4 () {
+      System.out.println("Thrid Method is Working Now");
+    }
+
+    // Main Method
+
+    public static void main (String arg[]){
+        Two check = new Two();
+        check.m2();
+        check.m3();
+        check.m4();
+    }
+ }
---
+++ Variables/VariablesDemo2.java
@@ -0,0 +1,16 @@
+
+
+class VariablesDemo2 {
+ String myName = "Vijay";
+ int myAge = 27;
+ double myFootSize = 10.00;
+ char myGrade = 'C';
+ boolean isMarride = false ;
+
+  public static void  main (String[] arg) {
+      VariablesDemo2 myData = new VariablesDemo2();
+      System.out.println("my Age is " +  myData.myAge);
+  }
+
+
+}
